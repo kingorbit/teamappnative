d20ff7733e2bfb2958019795c6d2072c3aeb5842
kingorbit <ghause6@gmail.com>
2023-10-31 18:34:25 +0100

    start 0.1

--- app/index.js
+++ app/index.js
@@ -1,11 +1,57 @@
-import {View, Text} from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const styles = {
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#9091fd',
+    },
+    menuItem: {
+      padding: 20,
+      margin: 10,
+      backgroundColor: 'white',
+      borderRadius: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      elevation: 4,
+      width: '80%', // Szerokość 80% szerokości ekranu
+    },
+    menuItemText: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+  };
 
 const Home = () => {
+    const handleCalendarPress = () => {
+      console.log('Przełączono na zakładkę Kalendarz');
+    };
+  
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
-    )
-}
-
-export default Home;
+      <View style={styles.container}>
+        <Text style={{fontSize: 24, color: 'white', fontWeight: 'bold', marginBottom: 20}}>Team App</Text>
+        <TouchableOpacity style={styles.menuItem} onPress={handleCalendarPress}>
+          <Text style={styles.menuItemText}>Kalendarz</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem} onPress={handleCalendarPress}>
+          <Text style={styles.menuItemText}> Drużyna </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem} onPress={handleCalendarPress}>
+          <Text style={styles.menuItemText}>Chat</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem} onPress={handleCalendarPress}>
+          <Text style={styles.menuItemText}>Statystyki</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem} onPress={handleCalendarPress}>
+          <Text style={styles.menuItemText}>Profil</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem} onPress={handleCalendarPress}>
+          <Text style={styles.menuItemText}>Ustawienia</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+  
+  export default Home;
---
+++ styles/styles.js
@@ -0,0 +1,25 @@
+// styles.js
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9091fd', // Ustawienie koloru tła
+  },
+  menuItem: {
+    padding: 20,
+    margin: 10,
+    backgroundColor: 'white', // Tło przycisku
+    borderRadius: 10, // Zaokrąglenie krawędzi
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 4, // Cień przycisku
+  },
+  menuItemText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+};
+
+export default styles;
