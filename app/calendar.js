2c92a8d34a4851b349bf7934785484cf91b559a0
kingorbit <ghause6@gmail.com>
2023-12-01 00:41:09 +0100

    start 1.5 - walka z kalendarzem

--- app/calendar.js
+++ app/calendar.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput, ScrollView } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import Header from '../components/header';
-import { collection, getDocs, query, where, addDoc } from 'firebase/firestore';
+import { collection, getDocs, query, where, addDoc, deleteDoc, doc, updateDoc, getDoc } from 'firebase/firestore';
 import { onAuthStateChanged } from 'firebase/auth';
 import { firestore, auth } from '../constants/config';
 import { useNavigate } from 'react-router-native';
@@ -10,7 +10,6 @@ import NavigationBar from '../components/navBar';
 import { LocaleConfig } from 'react-native-calendars';
 import ModalDropdown from 'react-native-modal-dropdown';
 import Icon from 'react-native-vector-icons/FontAwesome'; 
-import DatePicker from 'react-native-datepicker';
 
 LocaleConfig.locales['pl'] = {
   monthNames: [
@@ -67,6 +66,10 @@ const CalendarScreen = () => {
   const [markedDates, setMarkedDates] = useState({});
   const [selectedEventDetails, setSelectedEventDetails] = useState(null);
   const navigate = useNavigate();
+  const [isDeleteConfirmationVisible, setDeleteConfirmationVisible] = useState(false);
+  const [isEditFormVisible, setEditFormVisible] = useState(false);
+  const [editedEvent, setEditedEvent] = useState(null);
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -173,7 +176,6 @@ const CalendarScreen = () => {
   
     fetchData();
   }, []);
-
   const addEvent = async () => {
     try {
       if (!eventCategory || !eventName || !eventDate) {
@@ -204,6 +206,10 @@ const CalendarScreen = () => {
           eventData: newEvent,
         },
       }));
+      setEventName(''); // Wyczyszczenie pola eventName
+      setEventCategory(''); // Wyczyszczenie pola eventCategory
+      setEventDate(''); // Wyczyszczenie pola eventDate
+    // Możesz dodać więcej pól, jeśli to konieczne
 
       setEventFormVisible(false);
     } catch (error) {
@@ -212,12 +218,130 @@ const CalendarScreen = () => {
   };
 
   const showEventDetails = (event) => {
+    setEditedEvent(event);
     setSelectedEventDetails(event);
   };
+  
+  const updateEvent = async () => {
+    try {
+      if (editedEvent && editedEvent.id) {
+        const eventsRef = collection(firestore, 'events');
+        const eventDocRef = doc(eventsRef, editedEvent.id);
+  
+        // Sprawdź, czy dokument istnieje przed aktualizacją
+        const docSnapshot = await getDoc(eventDocRef);
+        if (docSnapshot.exists()) {
+          await updateDoc(eventDocRef, {
+            eventName: editedEvent.eventName,
+            eventCategory: editedEvent.eventCategory,
+            eventDate: editedEvent.eventDate,
+            // Dodaj inne pola, jeśli są potrzebne
+          });
+  
+          // Zaktualizuj stan lokalny
+          const updatedEvents = events.map((event) =>
+            event.id === editedEvent.id ? { ...event, ...editedEvent } : event
+          );
+          setEvents(updatedEvents);
+  
+          // Zaktualizuj markedDates, jeśli data się zmieniła
+          if (editedEvent.eventDate !== selectedEventDetails.eventDate) {
+            const updatedMarkedDates = { ...markedDates };
+            delete updatedMarkedDates[selectedEventDetails.eventDate.split(' ')[0]];
+            updatedMarkedDates[editedEvent.eventDate.split(' ')[0]] = {
+              selected: true,
+              marked: true,
+              dotColor: 'black',
+              eventData: editedEvent,
+            };
+            setMarkedDates(updatedMarkedDates);
+          }
+  
+          setEditedEvent(null);
+          setEditFormVisible(false);
+        } else {
+          console.error('Dokument nie istnieje.');
+        }
+      }
+    } catch (error) {
+      console.error('Błąd aktualizacji wydarzenia', error);
+    }
+  };
+
+  const deleteEvent = async () => {
+    try {
+      if (selectedEventDetails && selectedEventDetails.id) {
+        // Usuń zdarzenie z Firestore na podstawie identyfikatora dokumentu
+        const eventsRef = collection(firestore, 'events');
+        await deleteDoc(doc(eventsRef, selectedEventDetails.id));
+  
+        // Usuń zdarzenie ze stanu lokalnego
+        const updatedEvents = events.filter(
+          (event) => event.id !== selectedEventDetails.id
+        );
+        setEvents(updatedEvents);
+  
+        // Usuń zdarzenie z markedDates
+        const updatedMarkedDates = { ...markedDates };
+        delete updatedMarkedDates[selectedEventDetails.eventDate.split(' ')[0]];
+  
+        setMarkedDates(updatedMarkedDates);
+  
+        // Zamknij modal potwierdzenia usunięcia
+        setDeleteConfirmationVisible(false);
+        // Zamknij modal z szczegółami wydarzenia
+        setSelectedEventDetails(null);
+      }
+    } catch (error) {
+      console.error('Błąd usuwania zdarzenia', error);
+    }
+  };
+  const renderUpcomingEvents = () => {
+    const upcomingEvents = events
+      .filter((event) => new Date(event.eventDate) >= new Date())
+      .sort((a, b) => new Date(a.eventDate) - new Date(b.eventDate))
+      .slice(0, 5);
+  
+    return (
+      <ScrollView style={styles.upcomingEventsContainer}>
+        <Text style={styles.sectionTitle}>Najbliższe wydarzenia:</Text>
+        {upcomingEvents.map((event) => (
+          <TouchableOpacity
+            key={event.id}
+            style={styles.upcomingEventButton}
+            onPress={() => showEventDetails(event)}
+          >
+            <View style={styles.upcomingEventDetails}>
+              <Text style={styles.upcomingEventText}>{event.eventName}</Text>
+              <Text>Kategoria: {event.eventCategory}</Text>
+              <Text>Data: {event.eventDate}</Text>
+            </View>
+            <View style={styles.daysRemainingContainer}>
+              <Text style={styles.daysRemainingText}>{calculateDaysRemaining(event.eventDate)}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    );
+  };
+  
+  // Funkcja do obliczania pozostałych dni do zdarzenia
+  const calculateDaysRemaining = (eventDate) => {
+    const today = new Date();
+    const eventDateTime = new Date(eventDate);
+  
+    // Oblicz różnicę między dzisiaj a datą wydarzenia
+    const timeDifference = eventDateTime.getTime() - today.getTime();
+    const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+  
+    return daysRemaining > 0 ? `Za ${daysRemaining} dni` : 'Dziś';
+  };
+  
 
 
   return (
     <View style={styles.container}>
+          <ScrollView style={styles.childcontainer}>
       <Header user={user} setUser={setUser} />
       <Text style={styles.title}>Kalendarz</Text>
       <View style={styles.calendarcontainer}>
@@ -230,7 +354,6 @@ const CalendarScreen = () => {
       showEventDetails(selectedEvent);
     }
 
-    // Dodaj dynamiczną zmianę koloru tła w zależności od kategorii
     const categoryColor = categoryColors[selectedEvent?.eventCategory];
     if (categoryColor) {
       const updatedMarkedDates = { ...markedDates };
@@ -261,42 +384,13 @@ const CalendarScreen = () => {
     todayTextColor: 'green',
     dayTextColor: 'black',
     textDisabledColor: 'gray',
-    // Dostosuj kolory kropek w zależności od kategorii
-    markedDates: markedDates,
-    markingType: 'period',
-    periodStyles: {
-      // Dodaj tę sekcję
-      MeczPucharowy: {
-        selectedDayBackgroundColor: 'blue',
-        color: 'white',
-      },
-      MeczLigowy: {
-        selectedDayBackgroundColor: 'blue',
-        color: 'white',
-      },
-      Trening: {
-        selectedDayBackgroundColor: 'blue',
-        color: 'white',
-      },
-      // Dodaj inne kategorie według potrzeb
-    },
   }}
 />
         {user && isCoach && (
           <View style={styles.buttonsContainer}>
             <TouchableOpacity style={styles.iconButton} onPress={() => setEventFormVisible(true)}>
               <Icon name="plus" size={17} color="black" />
-              <Text style={styles.buttonText}>Dodaj</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.iconButton}>
-              <Icon name="gear" size={17} color="black" />
-              <Text style={styles.buttonText}>Edytuj</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.iconButton}>
-              <Icon name="ban" size={17} color="black" />
-              <Text style={styles.buttonText}>Usuń</Text>
+              <Text style={styles.buttonText}>Dodaj Wydarzenie </Text>
             </TouchableOpacity>
           </View>
         )}
@@ -317,68 +411,167 @@ const CalendarScreen = () => {
         onChangeText={(text) => setEventName(text)}
       />
       <ModalDropdown
-        style={styles.input}
         options={categories}
+        style={styles.dropdown}
+        textStyle={styles.dropdownText}
+        dropdownStyle={styles.dropdownOptions}
+        dropdownTextStyle={styles.dropdownOptionText}
         defaultValue="Wybierz kategorię"
         onSelect={(index, value) => setEventCategory(value)}
       />
-      {/* Zastąp TextInput komponentem DatePicker */}
-      <DatePicker
-        style={styles.input}
-        date={eventDate}
-        mode="date"
-        placeholder="Wybierz datę"
-        format="YYYY-MM-DD"
-        minDate="2000-01-01"
-        maxDate="2100-12-31"
-        confirmBtnText="Potwierdź"
-        cancelBtnText="Anuluj"
-        customStyles={{
-          dateInput: {
-            borderWidth: 0,
-          },
-          // Możesz dostosować pozostałe style według potrzeb
-        }}
-        onDateChange={(date) => setEventDate(date)}
-      />
-      <TouchableOpacity style={styles.addButton} onPress={addEvent}>
-        <Text style={styles.buttonText}>Dodaj</Text>
+<TextInput
+  style={styles.input}
+  placeholder="Wybierz datę"
+  value={eventDate}
+  onChangeText={(text) => {
+    // Sprawdź, czy tekst zawiera tylko dozwolone znaki
+    const sanitizedText = text.replace(/[^0-9.\-]/g, ''); // Pozbądź się niedozwolonych znaków
+
+    // Sprawdź, czy liczba kropek jest mniejsza niż 2, a my musimy mieć co najmniej jedno cyfrowe
+    if (
+      sanitizedText.split('.').length <= 2 &&
+      /^[0-9]*([.\-]?[0-9]*)*$/.test(sanitizedText)
+    ) {
+      setEventDate(sanitizedText);
+    }
+  }}
+  keyboardType="numeric"
+/>
+      <TouchableOpacity style={[styles.addButton, { backgroundColor: '#4BB543' }]} onPress={addEvent}>
+   <Text style={[styles.buttonText, { color: 'white' }]}>Dodaj</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        style={styles.closeButton}
-        onPress={() => setEventFormVisible(false)}
+  style={[styles.closeButton, { backgroundColor: 'red' }]} // Dodano styl o kolorze czerwonym
+  onPress={() => setEventFormVisible(false)}
+>
+<Text style={[styles.buttonText, { color: 'white' }]}>Zamknij</Text>
+</TouchableOpacity>
+    </View>
+  </View>
+</Modal>
+
+<Modal
+  visible={!!selectedEventDetails}
+  transparent={true}
+  animationType="slide"
+  onRequestClose={() => {
+    setSelectedEventDetails(null);
+    setEditFormVisible(false);
+  }}
+>
+  <View style={styles.modalContainer}>
+    <View style={styles.modalContent}>
+      {selectedEventDetails && (
+        <View>
+          <Text style={styles.modalTitle}>Szczegóły</Text>
+          <Text>Nazwa: {selectedEventDetails.eventName}</Text>
+          <Text>Kategoria: {selectedEventDetails.eventCategory}</Text>
+          <Text>Data: {selectedEventDetails.eventDate}</Text>
+          {isCoach && (
+            <View>
+              <TouchableOpacity
+                style={[styles.deleteButton, { backgroundColor: 'red' }]}
+                onPress={() => setDeleteConfirmationVisible(true)}
+              >
+                <Text style={[styles.buttonText, { color: 'white' }]}>Usuń</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.editButton, { backgroundColor: 'orange' }]}
+                onPress={() => {
+                  setEditFormVisible(true);
+                  setDeleteConfirmationVisible(false);
+                  setSelectedEventDetails(null);
+                }}
+              >
+                <Text style={[styles.buttonText, { color: 'white' }]}>Edytuj</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+          <TouchableOpacity
+            style={[styles.closeButton, { backgroundColor: '#198754' }]}
+            onPress={() => setSelectedEventDetails(null)}
+          >
+            <Text style={[styles.buttonText, { color: 'white' }]}>Zamknij</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  </View>
+</Modal>
+<Modal
+  visible={isDeleteConfirmationVisible}
+  transparent={true}
+  animationType="slide"
+  onRequestClose={() => setDeleteConfirmationVisible(false)}
+>
+  <View style={styles.modalContainer}>
+    <View style={styles.modalContent}>
+      <Text style={styles.modalTitle}>Potwierdź Usunięcie</Text>
+      <TouchableOpacity
+        style={[styles.confirmDeleteButton, { backgroundColor: 'red' }]}
+        onPress={deleteEvent}
       >
-        <Text style={styles.buttonText}>Anuluj</Text>
+        <Text style={[styles.buttonText, { color: 'white' }]}>Usuń</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.closeButton, { backgroundColor: '#198754' }]}
+        onPress={() => setDeleteConfirmationVisible(false)}
+      >
+        <Text style={[styles.buttonText, { color: 'white' }]}>Anuluj</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+</Modal>
+<Modal
+  visible={isEditFormVisible}
+  transparent={true}
+  animationType="slide"
+  onRequestClose={() => setEditFormVisible(false)}
+>
+  <View style={styles.modalContainer}>
+    <View style={styles.modalContent}>
+      <Text style={styles.modalTitle}>Edytuj Wydarzenie</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nazwa wydarzenia"
+        value={editedEvent?.eventName}
+        onChangeText={(text) => setEditedEvent((prev) => ({ ...prev, eventName: text }))}
+      />
+      <ModalDropdown
+        options={categories}
+        style={styles.dropdown}
+        textStyle={styles.dropdownText}
+        dropdownStyle={styles.dropdownOptions}
+        dropdownTextStyle={styles.dropdownOptionText}
+        defaultValue={editedEvent?.eventCategory || "Wybierz kategorię"}
+        onSelect={(index, value) => setEditedEvent((prev) => ({ ...prev, eventCategory: value }))}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Wybierz datę"
+        value={editedEvent?.eventDate}
+        onChangeText={(text) => setEditedEvent((prev) => ({ ...prev, eventDate: text }))}
+        keyboardType="numeric"
+      />
+      <TouchableOpacity
+        style={[styles.addButton, { backgroundColor: '#4BB543' }]}
+        onPress={updateEvent}
+      >
+        <Text style={[styles.buttonText, { color: 'white' }]}>Zapisz zmiany</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.closeButton, { backgroundColor: 'red' }]}
+        onPress={() => setEditFormVisible(false)}
+      >
+        <Text style={[styles.buttonText, { color: 'white' }]}>Anuluj</Text>
       </TouchableOpacity>
     </View>
   </View>
 </Modal>
 
-      <Modal
-        visible={!!selectedEventDetails}
-        transparent={true}
-        animationType="slide"
-        onRequestClose={() => setSelectedEventDetails(null)}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            {selectedEventDetails && (
-              <View>
-                <Text>Nazwa: {selectedEventDetails.eventName}</Text>
-                <Text>Kategoria: {selectedEventDetails.eventCategory}</Text>
-                <Text>Data: {selectedEventDetails.eventDate}</Text>
-                <TouchableOpacity
-                  style={styles.closeButton}
-                  onPress={() => setSelectedEventDetails(null)}
-                >
-                  <Text style={styles.buttonText}>Zamknij</Text>
-                </TouchableOpacity>
-              </View>
-            )}
-          </View>
-        </View>
-      </Modal>
       </View>
+      {renderUpcomingEvents()}
+      </ScrollView>
       <NavigationBar></NavigationBar>
     </View>
   );
@@ -420,7 +613,7 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     margin: 10,
-    width: '50%',
+    width: 75,
     alignSelf: 'center',
   },
   buttonText: {
@@ -435,7 +628,7 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     backgroundColor: 'white',
-    padding: 20,
+    padding: 25,
     borderRadius: 10,
     elevation: 5,
   },
@@ -443,14 +636,15 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     padding: 10,
     borderRadius: 5,
-    margin: 10,
+    marginTop: 20,
     alignSelf: 'center',
+    width: 75,
   },
   modalTitle: {
-    fontSize: 20,
+    fontSize: 25,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: 10,
+    marginBottom: 15,
   },
   input: {
     height: 40,
@@ -458,7 +652,98 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
+    borderRadius: 5,
   },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    color: 'white',
+    alignSelf: 'center',
+  },
+
+  upcomingEventsContainer: {
+    margin: 10,
+    padding: 20,
+  },
+
+  upcomingEventButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 5,
+  },
+  upcomingEventDetails: {
+    flex: 1,
+  },
+  upcomingEventText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  daysRemainingContainer: {
+    backgroundColor: 'green',
+    borderRadius: 5,
+    padding: 10,
+    marginLeft: 10,
+    width: '27%',
+    height: 40,
+  },
+  daysRemainingText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  dropdown: {
+    height: 35,
+    borderWidth: 1,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    marginBottom: 15,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  dropdownText: {
+    textAlign: 'center',
+    fontSize: 13
+    ,
+  },
+  dropdownOptions: {
+    textAlign: 'center',
+    width: '50%',
+    borderRadius: 5,
+  },
+  dropdownOptionText: {
+    textAlign: 'center',
+    fontSize: 15,
+  },
+  deleteButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    alignSelf: 'center',
+    width: 75,
+  },
+  editButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    alignSelf: 'center',
+    width: 75,
+  },
+  confirmDeleteButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    alignSelf: 'center',
+    width: 75,
+    marginBottom: 10,
+  }
 });
 
 export default CalendarScreen;
